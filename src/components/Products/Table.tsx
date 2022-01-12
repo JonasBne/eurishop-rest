6818dab0b9094914ec37d5671756812a82cc0c70
JONAS <jonas.boone@euri.com>
2022-01-12 16:31:10 +0100

    First version of table with products (no styling)

--- src/components/Products/Table.tsx
+++ src/components/Products/Table.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import ProductsContext, {
   ProductsContextProps,
 } from "../../store/ProductsContext";
+import Product from "../../types/Product";
 
 function Table() {
   const { loading, error, products } =
@@ -12,7 +13,34 @@ function Table() {
       {loading && <div>Loading...</div>}
       {!loading && products.length <= 0 && <div>Loading...</div>}
       {error && <div>Error...</div>}
-      {!loading && !error && <div>Success...</div>}
+      {!loading && !error && (
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Image</th>
+              <th>Stock</th>
+              <th>Baseprice</th>
+              <th>price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product: Product) => {
+              return (
+                <tr key={product.id}>
+                  <td>{product.title}</td>
+                  <td>{product.desc}</td>
+                  <td>{product.image}</td>
+                  <td>{product.stocked}</td>
+                  <td>{product.basePrice}</td>
+                  <td>{product.price}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </>
   );
 }
