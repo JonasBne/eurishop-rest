b0efbaa0f14ec934f67e221ca87d8c7f371a63f8
JONAS <jonas.boone@euri.com>
2022-01-12 10:00:17 +0100

    Added Navbar to other components

--- src/routes/ProductDetail.tsx
+++ src/routes/ProductDetail.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Navbar from "../components/general/Navbar";
 
 function ProductDetail() {
-  return <div>Product detail goes here</div>;
+  return (
+    <>
+      <Navbar />
+      <div>PRODUCT DETAIL HERE</div>
+    </>
+  );
 }
 
 export default ProductDetail;
--- src/routes/ProductOverview.tsx
+++ src/routes/ProductOverview.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Navbar from "../components/general/Navbar";
 
 function ProductOverview() {
-  return <div>Product overview for end user goes here</div>;
+  return (
+    <>
+      <Navbar />
+      <div>PRODUCT OVERVIEW FOR USER HERE</div>
+    </>
+  );
 }
 
 export default ProductOverview;
