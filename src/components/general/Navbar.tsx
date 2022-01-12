1017d82dcd829843cae23a7342967ee5d0ecaba7
JONAS <jonas.boone@euri.com>
2022-01-12 09:58:20 +0100

    Added basic styling on Navbar component

--- src/components/general/Navbar.tsx
+++ src/components/general/Navbar.tsx
@@ -1,21 +1,53 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { space, SpaceProps } from "styled-system";
+
+const Nav = styled.nav`
+  background-color: #1d3557;
+  display: flex;
+  align-items: center;
+`;
+
+const NavList = styled.ul`
+  list-style-type: none;
+`;
+
+const NavListItem = styled.li<SpaceProps>`
+  text-transform: uppercase;
+  display: inline-flex;
+  justify-content: space-around;
+  ${space}
+`;
+
+const NavLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+`;
+
+const Header1 = styled.h1<SpaceProps>`
+  color: white;
+  ${space}
+`;
 
 function Navbar() {
   return (
-    <nav>
-      <div>
-        <li>
-          <Link to="/home">Home</Link>
-        </li>
-        <li>
-          <Link to="/products">Products</Link>
-        </li>
-        <li>
-          <Link to="/products/admin">Products (admin)</Link>
-        </li>
-      </div>
-    </nav>
+    <Nav>
+      <Header1 ml="1rem" mr="2rem">
+        EURISHOP
+      </Header1>
+      <NavList>
+        <NavListItem mx="2rem">
+          <NavLink to="/home">Home</NavLink>
+        </NavListItem>
+        <NavListItem mx="2rem">
+          <NavLink to="/products">Products</NavLink>
+        </NavListItem>
+        <NavListItem mx="2rem">
+          <NavLink to="/products/admin">Products (admin)</NavLink>
+        </NavListItem>
+      </NavList>
+    </Nav>
   );
 }
 
