5b1f2cb2761ccf06c2b919da6ed1c3b5d9e42738
JONAS <jonas.boone@euri.com>
2022-01-12 10:36:32 +0100

    Fixed warning regarding deprecated legacy api and updated to React v18

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from "react";
-import { render } from "react-dom";
+import * as ReactDOM from "react-dom";
+// import { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import ProductDetail from "./routes/ProductDetail";
 import ProductOverview from "./routes/ProductOverview";
 import ProductTable from "./routes/ProductTable";
 
-const rootElement = document.getElementById("root");
-render(
+const container = document.getElementById("app");
+// @ts-ignore
+const root = ReactDOM.createRoot(container);
+
+root.render(
   <BrowserRouter>
     <Routes>
       <Route path="*" element={<p>There is nothing here!</p>} />
@@ -17,6 +21,20 @@ render(
       <Route path="products/:productId" element={<ProductDetail />} />
       <Route path="products/admin" element={<ProductTable />} />
     </Routes>
-  </BrowserRouter>,
-  rootElement
+  </BrowserRouter>
 );
+
+// const rootElement = document.getElementById("root");
+// render(
+//   <BrowserRouter>
+//     <Routes>
+//       <Route path="*" element={<p>There is nothing here!</p>} />
+//       <Route path="/" element={<App />} />
+//       <Route path="home" element={<ProductOverview />} />
+//       <Route path="products" element={<ProductOverview />} />
+//       <Route path="products/:productId" element={<ProductDetail />} />
+//       <Route path="products/admin" element={<ProductTable />} />
+//     </Routes>
+//   </BrowserRouter>,
+//   rootElement
+// );
