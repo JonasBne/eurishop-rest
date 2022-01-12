a2856dcd2164cb39052f6fa2c1d442967a6466f2
JONAS <jonas.boone@euri.com>
2022-01-12 15:39:03 +0100

    Cleanup index.tsx file

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import * as ReactDOM from "react-dom";
-// import { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import ProductDetail from "./routes/ProductDetail";
