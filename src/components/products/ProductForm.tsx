cd910f7a18c62caa7852f58d17cb7b1e3fbe4a89
JONAS <jonas.boone@euri.com>
2022-01-13 16:28:42 +0100

    Added labels

--- src/components/products/ProductForm.tsx
+++ src/components/products/ProductForm.tsx
@@ -8,12 +8,37 @@ interface ProductFormProps {
 function ProductForm({ product }: ProductFormProps) {
   return (
     <form>
-      <input type="text" defaultValue={product.sku} />
-      <input type="text" defaultValue={product.title} />
-      <input type="text" defaultValue={product.desc} />
-      <input type="text" defaultValue={product.stocked ? "Yes" : "No"} />
-      <input type="text" defaultValue={product.basePrice} />
-      <input type="text" defaultValue={product.price} />
+      <label htmlFor="number">
+        Number
+        <input id="number" type="text" defaultValue={product.sku} />
+      </label>
+
+      <label htmlFor="title">
+        Title <input id="title" type="text" defaultValue={product.title} />
+      </label>
+
+      <label htmlFor="description">
+        Description
+        <input id="description" type="text" defaultValue={product.desc} />
+      </label>
+
+      <label htmlFor="stocked">
+        In stock
+        <input
+          id="stocked"
+          type="text"
+          defaultValue={product.stocked ? "Yes" : "No"}
+        />
+      </label>
+
+      <label htmlFor="base-price">
+        Base price
+        <input id="base-price" type="text" defaultValue={product.basePrice} />
+      </label>
+
+      <label htmlFor="price">
+        Unit price <input id="price" type="text" defaultValue={product.price} />
+      </label>
     </form>
   );
 }
