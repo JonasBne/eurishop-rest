ff37e496f79a033154e03762290d50fcaa1c74a8
JONAS <jonas.boone@euri.com>
2022-01-13 09:44:49 +0100

    Refactored code to hide navbar on the PageNotFound component

--- src/routes/ProductTableAdmin.tsx
+++ src/routes/ProductTableAdmin.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navbar from "../components/general/Navbar";
 import ProductTable from "../components/products/ProductTable";
+import ProductsContext, {
+  ProductsContextProps,
+} from "../store/ProductsContext";
 
 function ProductTableAdmin() {
+  const { error } = useContext<ProductsContextProps>(ProductsContext);
   return (
     <>
-      <Navbar />
+      {!error && <Navbar />}
       <ProductTable />
     </>
   );
