0124ae60581fc945e0302bc56e134764e80a627d
JONAS <jonas.boone@euri.com>
2022-01-13 15:29:41 +0100

    Added useParams to retrieve productId from URL

--- src/routes/ProductDetail.tsx
+++ src/routes/ProductDetail.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { useParams } from "react-router";
 import Navbar from "../components/general/Navbar";
 
 function ProductDetail() {
+  const { productId } = useParams<string>();
+
   return (
     <>
       <Navbar />
-      <div>PRODUCT DETAIL HERE</div>
+      <div>{`DETAIL OF PRODUCT WITH ID ${productId}`}</div>
     </>
   );
 }
