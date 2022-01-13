feda613e7769c35fb27516a6098368ed70959a91
JONAS <jonas.boone@euri.com>
2022-01-13 15:44:00 +0100

    Added code to fetch product data and first building block of JSX

--- src/routes/ProductDetail.tsx
+++ src/routes/ProductDetail.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useParams } from "react-router";
 import Navbar from "../components/general/Navbar";
+import ProductsContext, {
+  ProductsContextProps,
+} from "../store/ProductsContext";
+import LoadingSpinner from "../components/general/LoadingSpinner";
+import PageNotFound from "../components/general/PageNotFound";
 
 function ProductDetail() {
   const { productId } = useParams<string>();
+  const { loading, error, products } =
+    useContext<ProductsContextProps>(ProductsContext);
+  const product = products.find((prod) => prod.id.toString() === productId);
 
   return (
     <>
       <Navbar />
-      <div>{`DETAIL OF PRODUCT WITH ID ${productId}`}</div>
+      {loading && !error && <LoadingSpinner />}
+      {error && <PageNotFound />}
+      {!loading && !error && (
+        <h3>{`Product detail: ${product?.title} (${product?.sku})`}</h3>
+      )}
     </>
   );
 }
