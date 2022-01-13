26d2944955fb2d58f269c73ad99d40d21d8ff681
JONAS <jonas.boone@euri.com>
2022-01-13 14:01:14 +0100

    Added a custom hook to extract sorting logic from ProductTable component and increase reusability

---
+++ src/hooks/useSortTableData.ts
@@ -0,0 +1,53 @@
+import { useState, useMemo } from "react";
+import Product from "../types/Product";
+
+interface SortConfig {
+  key: string;
+  order: string;
+}
+
+function useSortTableData(data: Product[], key: string) {
+  const [sortConfig, setSortConfig] = useState<SortConfig | null>(null);
+
+  function requestedSorting() {
+    let order = "default";
+    if (sortConfig?.key === key && sortConfig.order === "default") {
+      order = "ascending";
+    }
+    if (sortConfig?.key === key && sortConfig.order === "ascending") {
+      order = "descending";
+    }
+    if (sortConfig?.key === key && sortConfig.order === "descending") {
+      order = "default";
+    }
+    setSortConfig({ key, order });
+  }
+
+  const sortedData = [...data];
+
+  useMemo(() => {
+    if (sortConfig !== null && sortConfig.order !== "default") {
+      sortedData.sort((a: any, b: any) => {
+        // TODO: find a better alternative to typecast this
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.order === "ascending" ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.order === "ascending" ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+    if (sortConfig !== null && sortConfig.order === "default") {
+      sortedData.sort((a: any) => a);
+    }
+    return sortedData;
+  }, [data, sortConfig]);
+
+  return {
+    requestedSorting,
+    sortedData,
+  };
+}
+
+export default useSortTableData;
