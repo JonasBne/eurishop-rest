441fd25c0ac61298572fc88975e9a8704cc7c84d
JONAS <jonas.boone@euri.com>
2022-01-13 16:48:42 +0100

    Added basic styling to ProductForm

--- src/components/products/ProductForm.tsx
+++ src/components/products/ProductForm.tsx
@@ -1,5 +1,34 @@
 import React from "react";
+import styled from "styled-components";
+import { layout, space, SpaceProps, LayoutProps } from "styled-system";
 import Product from "../../types/Product";
+
+const Form = styled.form<SpaceProps | LayoutProps>`
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.22);
+  -moz-box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.22);
+  -webkit-box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.22);
+  ${space}
+  ${layout}
+`;
+
+const Label = styled.label<SpaceProps>`
+  ${space}
+`;
+
+const Input = styled.input`
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  outline: none;
+  display: block;
+  width: 100%;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  background: transparent;
+  height: 2rem;
+`;
 
 interface ProductFormProps {
   product: Product;
@@ -7,52 +36,52 @@ interface ProductFormProps {
 
 function ProductForm({ product }: ProductFormProps) {
   return (
-    <form>
-      <label htmlFor="number">
+    <Form p="1rem" width="fit-content">
+      <Label htmlFor="number" m="1rem">
         Number
-        <input id="number" type="text" defaultValue={product.sku} disabled />
-      </label>
+        <Input id="number" type="text" defaultValue={product.sku} disabled />
+      </Label>
 
-      <label htmlFor="title">
+      <Label htmlFor="title" m="1rem">
         Title{" "}
-        <input id="title" type="text" defaultValue={product.title} disabled />
-      </label>
+        <Input id="title" type="text" defaultValue={product.title} disabled />
+      </Label>
 
-      <label htmlFor="description">
+      <Label htmlFor="description" m="1rem">
         Description
-        <input
+        <Input
           id="description"
           type="text"
           defaultValue={product.desc}
           disabled
         />
-      </label>
+      </Label>
 
-      <label htmlFor="stocked">
+      <Label htmlFor="stocked" m="1rem">
         In stock
-        <input
+        <Input
           id="stocked"
           type="text"
           defaultValue={product.stocked ? "Yes" : "No"}
           disabled
         />
-      </label>
+      </Label>
 
-      <label htmlFor="base-price">
+      <Label htmlFor="base-price" m="1rem">
         Base price
-        <input
+        <Input
           id="base-price"
           type="text"
           defaultValue={product.basePrice}
           disabled
         />
-      </label>
+      </Label>
 
-      <label htmlFor="price">
+      <Label htmlFor="price" m="1rem">
         Unit price{" "}
-        <input id="price" type="text" defaultValue={product.price} disabled />
-      </label>
-    </form>
+        <Input id="price" type="text" defaultValue={product.price} disabled />
+      </Label>
+    </Form>
   );
 }
 
