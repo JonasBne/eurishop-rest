eda24fc57c1dcee600a5a9e05ed25c6e4fd99a41
JONAS <jonas.boone@euri.com>
2022-01-13 16:02:48 +0100

    Renamed folder

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import * as ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import ProductDetail from "./routes/ProductDetail";
-import ProductOverview from "./routes/ProductOverview";
-import ProductTableAdmin from "./routes/ProductTableAdmin";
+import ProductDetail from "./views/ProductDetail";
+import ProductOverview from "./views/ProductOverview";
+import ProductTableAdmin from "./views/ProductTableAdmin";
 import ProductsContextProvider from "./store/ProductsContextProvider";
 
 const container = document.getElementById("app");
