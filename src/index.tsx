696225b97bbb553374ec20614e128da311442e32
JONAS <jonas.boone@euri.com>
2022-01-13 09:43:30 +0100

    Moved ProductsContextProvider

--- src/index.tsx
+++ src/index.tsx
@@ -5,20 +5,23 @@ import App from "./App";
 import ProductDetail from "./routes/ProductDetail";
 import ProductOverview from "./routes/ProductOverview";
 import ProductTableAdmin from "./routes/ProductTableAdmin";
+import ProductsContextProvider from "./store/ProductsContextProvider";
 
 const container = document.getElementById("app");
 // @ts-ignore
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="*" element={<p>There is nothing here!</p>} />
-      <Route path="/" element={<App />} />
-      <Route path="home" element={<ProductOverview />} />
-      <Route path="products" element={<ProductOverview />} />
-      <Route path="products/:productId" element={<ProductDetail />} />
-      <Route path="products/admin" element={<ProductTableAdmin />} />
-    </Routes>
-  </BrowserRouter>
+  <ProductsContextProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<p>There is nothing here!</p>} />
+        <Route path="/" element={<App />} />
+        <Route path="home" element={<ProductOverview />} />
+        <Route path="products" element={<ProductOverview />} />
+        <Route path="products/:productId" element={<ProductDetail />} />
+        <Route path="products/admin" element={<ProductTableAdmin />} />
+      </Routes>
+    </BrowserRouter>
+  </ProductsContextProvider>
 );
--- src/routes/ProductTableAdmin.tsx
+++ src/routes/ProductTableAdmin.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Navbar from "../components/general/Navbar";
 import ProductTable from "../components/products/ProductTable";
-import ProductsContextProvider from "../store/ProductsContextProvider";
 
 function ProductTableAdmin() {
   return (
-    <ProductsContextProvider>
+    <>
       <Navbar />
       <ProductTable />
-    </ProductsContextProvider>
+    </>
   );
 }
 
