f719275d329e84ad43c12f17b72e8035f7d0abc5
JONAS <jonas.boone@euri.com>
2022-01-13 14:13:16 +0100

    Cleanup of ProductTable.tsx file

--- src/components/products/ProductTable.tsx
+++ src/components/products/ProductTable.tsx
@@ -1,9 +1,4 @@
 import React, { useContext } from "react";
-// import styled from "styled-components";
-// import { space, SpaceProps, layout, LayoutProps } from "styled-system";
-// TODO: error thrown by TS: 'can't resolve @fortawesome/react-fontawesome or it's type declarations' but packages are installed?
-// @ts-ignore
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPen,
   faTrash,
@@ -27,49 +22,6 @@ import {
   TableHead,
   TableRowHeader,
 } from "./ProductTable.styles";
-
-// const TableContainer = styled.table<SpaceProps>`
-//   ${space};
-//   font-size: 0.9rem;
-//   box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px,
-//     rgba(0, 0, 0, 0.22) 0px 15px 12px;
-// `;
-
-// const TableRowHeader = styled.tr`
-//   color: white;
-// `;
-
-// const TableHead = styled.th<SpaceProps | LayoutProps>`
-//   background-color: #0077b6;
-//   ${space}
-//   ${layout}
-//   &:hover {
-//     cursor: pointer;
-//   }
-// `;
-
-// const TableData = styled.td<SpaceProps>`
-//   ${space}
-// `;
-
-// const IconsContainer = styled.td<SpaceProps>`
-//   display: flex;
-//   justify-content: space-around;
-//   ${space}
-// `;
-
-// const Icon = styled(FontAwesomeIcon)`
-//   &:hover {
-//     cursor: pointer;
-//   }
-// `;
-
-// const SortIcon = styled(FontAwesomeIcon)<SpaceProps>`
-//   &:hover {
-//     cursor: pointer;
-//   }
-//   ${space}
-// `;
 
 function ProductTable() {
   const { loading, error, products } =
