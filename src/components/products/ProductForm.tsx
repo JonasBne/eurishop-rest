272e70f5651eca2719042ca9d692072cc36cf3b7
JONAS <jonas.boone@euri.com>
2022-01-14 09:56:36 +0100

    Minor changes to styling of textarea

--- src/components/products/ProductForm.tsx
+++ src/components/products/ProductForm.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import styled from "styled-components";
-import { layout, LayoutProps, space, SpaceProps } from "styled-system";
+import {
+  layout,
+  LayoutProps,
+  space,
+  SpaceProps,
+  typography,
+  TypographyProps,
+} from "styled-system";
 import Product from "../../types/Product";
 
-const Form = styled.form<LayoutProps>`
+const Form = styled.form<SpaceProps | LayoutProps>`
   ${layout}
+  ${space}
   display: grid;
   grid-template-areas:
     "number title . . "
     "stock basePrice . ."
     "unitPrice . . ."
     "desc desc desc desc";
-  box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.22);
-  -moz-box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.22);
   -webkit-box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.22);
 `;
 
@@ -58,8 +64,10 @@ const Input = styled.input`
   height: 2rem;
 `;
 
-const TextArea = styled.textarea<LayoutProps>`
+const TextArea = styled.textarea<SpaceProps | LayoutProps | TypographyProps>`
   ${layout}
+  ${space}
+  ${typography}
   box-sizing: border-box;
   -webkit-box-sizing: border-box;
   -moz-box-sizing: border-box;
@@ -78,7 +86,7 @@ interface ProductFormProps {
 // TODO: refactor code to grid
 function ProductForm({ product }: ProductFormProps) {
   return (
-    <Form height="30rem" width="40rem">
+    <Form height="30rem" width="40rem" p="1rem">
       <NumberLabel htmlFor="number" m="1rem">
         Number
         <Input id="number" type="text" defaultValue={product.sku} disabled />
@@ -116,7 +124,15 @@ function ProductForm({ product }: ProductFormProps) {
 
       <DescriptionLabel htmlFor="description" m="1rem">
         Description
-        <TextArea id="description" defaultValue={product.desc} disabled />
+        <TextArea
+          id="description"
+          defaultValue={product.desc}
+          disabled
+          width="100%"
+          height="100%"
+          mb="2rem"
+          lineHeight="1.5rem"
+        />
       </DescriptionLabel>
     </Form>
   );
