8fcc612c4514a9368e18b85e3e0accdf57f6f1f2
JONAS <jonas.boone@euri.com>
2022-01-17 09:51:06 +0100

    refactor(router): removed routes from index.tsx file
    BREAKING CHANGE: there is no longer a products context provider wrapped around the app component since state will be managed locally

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,14 @@
 import React from "react";
 import * as ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import ProductDetail from "./views/ProductDetail";
-import ProductOverview from "./views/ProductOverview";
-import ProductTableAdmin from "./views/ProductTableAdmin";
-import ProductsContextProvider from "./store/ProductsContextProvider";
 
 const container = document.getElementById("app");
 // @ts-ignore
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <ProductsContextProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<p>There is nothing here!</p>} />
-        <Route path="/" element={<App />} />
-        <Route path="home" element={<ProductOverview />} />
-        <Route path="products" element={<ProductOverview />} />
-        <Route path="products/:productId" element={<ProductDetail />} />
-        <Route path="products/admin" element={<ProductTableAdmin />} />
-      </Routes>
-    </BrowserRouter>
-  </ProductsContextProvider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
