10858de611f8b6def5c3f78cda309684cbcf2d62
JONAS <jonas.boone@euri.com>
2022-01-17 18:43:07 +0100

    refactor(productList): minor change to tableData

--- src/views/ProductList/ProductList.tsx
+++ src/views/ProductList/ProductList.tsx
@@ -7,14 +7,22 @@ import LoadingSpinner from "../../components/LoadingSpinner";
 function ProductList() {
   const { loading, error, products } = useGetProducts();
 
+  // TODO: is there a better way to avoid the ?. notation?
+  const tableData = products?.map((product) => {
+    return {
+      ...product,
+      stocked: product.stocked ? "Yes" : "No",
+    };
+  });
+
   return (
     <>
       {loading && !error && <LoadingSpinner />}
       {products === undefined && <LoadingSpinner />}
       {!loading && error && <div>Something went wrong...</div>}
       {/* TODO: is there a more clean way to check that products has been loaded and the array is available */}
-      {!loading && !error && products !== undefined && (
-        <Table data={products} />
+      {!loading && !error && tableData !== undefined && (
+        <Table data={tableData} />
       )}
     </>
   );
