5e425918e041816e68ce0f4a175763e91726ad31
JONAS <jonas.boone@euri.com>
2022-01-17 12:47:59 +0100

    style(index): added index.css to set margin of body to zero

--- src/views/Navigation/Navbar.tsx
+++ src/views/Navigation/Navbar.tsx
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import Burger from "./Burger";
 import Menu from "./Menu";
-// import { Link } from "react-router-dom";
-// import { space, SpaceProps } from "styled-system";
 import Header1 from "../../components/general/header/Header1";
 
 const StyledNav = styled.nav`
@@ -11,24 +9,7 @@ const StyledNav = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  border: 1px solid red;
 `;
-
-// const NavList = styled.ul`
-//   list-style-type: none;
-// `;
-
-// const NavListItem = styled.li<SpaceProps>`
-//   text-transform: uppercase;
-//   display: inline-flex;
-//   justify-content: space-around;
-//   ${space}
-// `;
-
-// const NavLink = styled(Link)`
-//   text-decoration: none;
-//   color: white;
-// `;
 
 function Navbar() {
   const [open, setOpen] = useState(false);
