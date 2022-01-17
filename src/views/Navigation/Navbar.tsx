4ed213d4cd9ab871d41d57ef1470e8f69869f00d
JONAS <jonas.boone@euri.com>
2022-01-17 13:13:01 +0100

    feat(menu): added a hook to close menu on click outside of the menu itself

--- src/views/Navigation/Navbar.tsx
+++ src/views/Navigation/Navbar.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, createRef } from "react";
 import styled from "styled-components";
 import Burger from "./Burger/Burger";
 import Menu from "./Menu/Menu";
 import Header1 from "../../components/general/header/Header1";
+import useOnClickOutside from "../../hooks/useOnClickOutside";
 
 const StyledNav = styled.nav`
   background: ${({ theme }) => theme.secondaryDark};
@@ -13,11 +14,13 @@ const StyledNav = styled.nav`
 
 function Navbar() {
   const [open, setOpen] = useState(false);
+  const node = createRef<HTMLDivElement>();
+  useOnClickOutside(node, () => setOpen(false));
 
   return (
     <StyledNav>
       <Header1 mx="auto">EURISHOP</Header1>
-      <div>
+      <div ref={node}>
         <Burger open={open} setOpen={setOpen} />
         <Menu open={open} />
       </div>
