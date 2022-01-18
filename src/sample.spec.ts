c012a4f34d8e46d641d89a536e79bc0eda27e69a
JONAS <jonas.boone@euri.com>
2022-01-18 16:46:12 +0100

    refactor: removed redundant file

--- src/sample.spec.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import path from "path";
-
-describe("my first test", () => {
-  test("test", () => {
-    expect(1).toBe(1);
-  });
-});
