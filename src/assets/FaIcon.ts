0323d6798420d5473eadc9a224a430f959f588d6
JONAS <jonas.boone@euri.com>
2022-01-18 15:57:58 +0100

    refactor(faIcon): changed props

--- src/assets/FaIcon.ts
+++ src/assets/FaIcon.ts
@@ -3,8 +3,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { space, SpaceProps } from "styled-system";
 
 const FaIcon = styled(FontAwesomeIcon)<SpaceProps>`
-  // color: ${({ theme }) => theme.secondaryDark};
-  color: red;
   ${space}
 `;
 
