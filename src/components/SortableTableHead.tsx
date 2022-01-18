ca07795029fd77d86a03b5c95a5bb479a5864349
JONAS <jonas.boone@euri.com>
2022-01-18 15:47:56 +0100

    refactor(table): moved styles for reusable table component inside a separate file

---
+++ src/components/SortableTableHead.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const SortableTableHead = () => {};
+
+export default SortableTableHead;
---
+++ src/components/Table.styles.ts
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+const StyledTable = styled.table`
+  border-spacing: 1;
+  border-collapse: collapse;
+  border-radius: 1rem;
+  overflow: hidden;
+  width: 100%;
+  margin: 0 auto;
+  box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px,
+    rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
+`;
+
+const TableHead = styled.th`
+  background: ${({ theme }) => theme.secondaryDark};
+  color: white;
+  padding: 1rem;
+  text-align: left;
+`;
+
+const TableRow = styled.tr`
+  height: 3.5rem;
+  color: #808080;
+  line-height: 1.2;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  &:nth-child(even) {
+    background-color: #f5f5f5;
+  }
+`;
+
+const TableData = styled.td`
+  width: fit-content;
+  padding: 1rem;
+  margin: 1rem;
+  text-align: center;
+`;
+
+export { StyledTable, TableHead, TableRow, TableData };
