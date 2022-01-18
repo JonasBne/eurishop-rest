6a5bff27b6f108af057709625651eaf69ead4e8c
JONAS <jonas.boone@euri.com>
2022-01-18 16:57:39 +0100

    refactor(theme): changed the theme object

--- src/theme.js
+++ src/theme.js
@@ -1,10 +1,35 @@
+// const theme = {
+//   primaryDark: "#012A4A",
+//   secondaryDark: "#013A63",
+//   primaryLight: "#2A6F97",
+//   secondaryLight: "A9D6E5",
+//   tertiaryLight: "#EFFFFA",
+//   primaryHover: "#343078",
+// };
+
+// export default theme;
+
 const theme = {
-  primaryDark: "#012A4A",
-  secondaryDark: "#013A63",
-  primaryLight: "#2A6F97",
-  secondaryLight: "A9D6E5",
-  tertiaryLight: "#EFFFFA",
-  primaryHover: "#343078",
+  colors: {
+    dark: [
+      {
+        primaryDarkBlue: "#012A4A",
+      },
+      { secondaryDarkBlue: "#013A63" },
+    ],
+    light: [
+      {
+        primaryLight: "#2A6F97",
+      },
+      {
+        secondaryLight: "A9D6E5",
+      },
+      { tertiaryLight: "#EFFFFA" },
+    ],
+  },
+  buttons: {
+    danger: "#A52A2A",
+  },
 };
 
 export default theme;
