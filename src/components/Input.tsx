54adf6431626c3702c846b168157bdab4b2cd2cc
JONAS <jonas.boone@euri.com>
2022-01-21 13:39:34 +0100

    refactor(input): changed input to a functional component so that it can be reused more easily

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,6 +1,7 @@
+import React from "react";
 import styled from "styled-components";
 
-const Input = styled.input`
+const StyledInput = styled.input`
   box-sizing: border-box;
   -webkit-box-sizing: border-box;
   -moz-box-sizing: border-box;
@@ -15,5 +16,15 @@ const Input = styled.input`
   word-break: break-all;
   width: 100%;
 `;
+
+interface InputProps {
+  id: string;
+  name: string;
+  type: string;
+}
+
+function Input({ id, name, type }: InputProps) {
+  return <StyledInput id={id} name={name} type={type} />;
+}
 
 export default Input;
