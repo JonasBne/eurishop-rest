49bfee20f5d2c3ca2a771c9c7c59fd285b4a2a55
JONAS <jonas.boone@euri.com>
2022-01-23 13:11:15 +0100

    style(input): removed redundant properties

--- src/components/Input.ts
+++ src/components/Input.ts
@@ -4,15 +4,9 @@ const Input = styled.input`
   box-sizing: border-box;
   -webkit-box-sizing: border-box;
   -moz-box-sizing: border-box;
-  outline: none;
-  display: block;
   border: none;
   border-bottom: 1px solid #ddd;
-  background: transparent;
-  height: 2rem;
-  min-width: 10rem;
-  word-wrap: break-word;
-  word-break: break-all;
+  height: 1rem;
   width: 100%;
   margin-top: 1rem;
 `;
