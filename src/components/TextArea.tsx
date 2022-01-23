6ce0a56545954935b31df4b27044e845160affbf
JONAS <jonas.boone@euri.com>
2022-01-23 12:43:37 +0100

    refactor(textarea): turned textarea into a styled component

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,9 +1,19 @@
-/* eslint-disable react/jsx-props-no-spreading */
-import React from "react";
+import styled from "styled-components";
 
-// TODO: make this a styled component
-function TextArea() {
-  return <textarea />;
-}
+const TextArea = styled.textarea`
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  outline: none;
+  display: block;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  background: transparent;
+  height: 2rem;
+  min-width: 10rem;
+  word-wrap: break-word;
+  word-break: break-all;
+  width: 100%;
+`;
 
 export default TextArea;
