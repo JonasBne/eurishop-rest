70d38a68a3890e639126c4d53d5514f8845171c4
JONAS <jonas.boone@euri.com>
2022-01-24 10:19:17 +0100

    refactor(button): cleanup

--- src/components/Button.ts
+++ src/components/Button.ts
@@ -1,8 +1,6 @@
 import styled from "styled-components";
 import { space, SpaceProps, layout, LayoutProps, variant } from "styled-system";
 
-// TODO: use variants
-
 type Variants = "primary" | "secondary" | "danger" | "success";
 
 interface VariantProps {
@@ -48,42 +46,5 @@ const Button = styled("button")<SpaceProps | LayoutProps | VariantProps>(
     },
   })
 );
-
-// const Button = styled.button<SpaceProps | LayoutProps>`
-//   ${space}
-//   ${layout}
-//   border-radius: 0.25rem;
-//   border-width: 0;
-//   box-shadow: rgba(50, 50, 93, 0.1) 0 0 0 1px inset,
-//     rgba(50, 50, 93, 0.1) 0 2px 5px 0, rgba(0, 0, 0, 0.07) 0 1px 1px 0;
-//   box-sizing: border-box;
-//   color: #fff;
-//   cursor: pointer;
-//   font-size: 100%;
-//   height: 2rem;
-//   line-height: 1.15;
-//   outline: none;
-//   padding: 0 25px;
-//   text-align: center;
-//   text-transform: none;
-
-//   &:hover {
-//     box-shadow: rgba(50, 50, 93, 0.1) 0 0 0 1px inset,
-//       rgba(50, 50, 93, 0.2) 0 6px 15px 0, rgba(0, 0, 0, 0.1) 0 2px 2px 0,
-//       rgba(50, 151, 211, 0.3) 0 0 0 4px;
-//   }
-//     ${variant}: {
-//       variants: {
-//         danger: {
-//           color: white,
-//           backgroundColor: "red,
-//         },
-//         primary: {
-//           color: white,
-//           backgroundColor: "green"
-//         }
-//       }
-//     }
-// `;
 
 export default Button;
