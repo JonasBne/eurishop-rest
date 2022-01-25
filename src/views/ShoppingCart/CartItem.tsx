a1cb11ac858d22a372447c4f1fd47ec64c180d4a
JONAS <jonas.boone@euri.com>
2022-01-25 13:50:51 +0100

    style(CartItem): changes to flex layout

--- src/views/ShoppingCart/CartItem.tsx
+++ src/views/ShoppingCart/CartItem.tsx
@@ -1,33 +1,43 @@
 import React from "react";
 import Header from "../../components/Header";
 import Button from "../../components/Button";
+import FlexBox from "../../components/FlexBox";
+import Box from "../../components/Box";
 
 // TODO: adjust types
 
 interface CartItemProps {
   item: any;
-  onRemove: (id: string) => void;
 }
 
-function CartItem({ item, onRemove }: CartItemProps) {
-  const handleRemove = (itemId: string) => {
-    onRemove(itemId);
-  };
+function CartItem({ item }: CartItemProps) {
+  // const handleRemove = (itemId: string) => {
+  //   onRemove(itemId);
+  // };
 
   return (
     <li>
-      <Header>{item.title}</Header>
       <div>
-        <span>{item.price}</span>
-        <span>{item.amount}</span>
+        <Header as="h3">{item.title}</Header>
+        <FlexBox flexDirection="column">
+          <img
+            src="https://dummyimage.com/300x300.jpg/ff4444/ffffff"
+            alt="product"
+            width="100px"
+            height="100px"
+          />
+          <Box my="0.5rem">Unit price: ${item.price}</Box>
+        </FlexBox>
       </div>
       {/* Is it ok in this stage that a products only gets added once? So there is no need for a + button? */}
       <Button
         type="button"
         variant="primary"
-        onClick={() => handleRemove(item.id)}
+        my="1rem"
+        // TODO: add the remove handler
+        onClick={() => alert("The product should be removed")}
       >
-        -
+        Remove
       </Button>
     </li>
   );
--- src/views/ShoppingCart/ShoppingCart.tsx
+++ src/views/ShoppingCart/ShoppingCart.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import styled from "styled-components";
 import Box from "../../components/Box";
 import FlexBox from "../../components/FlexBox";
 import Header from "../../components/Header";
+import CartItem from "./CartItem";
+
+// TODO: work with real data
 
 const DUMMY_DATA = [
   {
@@ -21,6 +25,10 @@ const DUMMY_DATA = [
   },
 ];
 
+const UnorderedList = styled.ul`
+  list-style-type: none;
+`;
+
 function ShoppingCart() {
   return (
     <Box
@@ -36,6 +44,11 @@ function ShoppingCart() {
           Current # of products in cart: {DUMMY_DATA.length}
         </Header>
       </FlexBox>
+      <UnorderedList>
+        {DUMMY_DATA.map((item) => {
+          return <CartItem item={item} />;
+        })}
+      </UnorderedList>
     </Box>
   );
 }
