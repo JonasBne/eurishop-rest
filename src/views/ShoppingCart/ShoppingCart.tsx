12c4a38fe63e5fb7e176546e2da235accfe8de2d
JONAS <jonas.boone@euri.com>
2022-01-25 11:56:46 +0100

    refactor(ShoppingCart): added headers

--- src/views/ShoppingCart/ShoppingCart.tsx
+++ src/views/ShoppingCart/ShoppingCart.tsx
@@ -1,16 +1,44 @@
 import React from "react";
-import styled from "styled-components";
+import Box from "../../components/Box";
+import FlexBox from "../../components/FlexBox";
+import Header from "../../components/Header";
 
-const StyledDiv = styled.div`
-  border: 1px solid black;
-  margin-right: 1rem;
-  width: 100%;
-  min-height: fit-content;
-  height: 50rem;
-`;
+const DUMMY_DATA = [
+  {
+    id: "1",
+    title: "product1",
+    price: 10.0,
+  },
+  {
+    id: "2",
+    title: "product2",
+    price: 8.0,
+  },
+  {
+    id: "3",
+    title: "product3",
+    price: 6.0,
+  },
+];
 
 function ShoppingCart() {
-  return <StyledDiv>BASKET</StyledDiv>;
+  return (
+    <Box
+      width="100%"
+      minHeight="fit-content"
+      height="50rem"
+      mr="1rem"
+      border="2px solid gray"
+    >
+      <FlexBox flexDirection="column" alignItems="center">
+        <Header as="h2">Shopping Cart</Header>
+        <Header
+          as="h3"
+          variant="tertiary"
+        >{`Current # of products in cart: ${DUMMY_DATA.length}`}</Header>
+      </FlexBox>
+    </Box>
+  );
 }
 
 export default ShoppingCart;
