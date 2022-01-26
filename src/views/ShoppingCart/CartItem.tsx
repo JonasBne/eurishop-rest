826438dd00e8973c111da11da16ab05852ebd780
JONAS <jonas.boone@euri.com>
2022-01-26 09:15:40 +0100

    refactor: removed styled list item and replaced with flexbox

--- src/views/ShoppingCart/CartItem.tsx
+++ src/views/ShoppingCart/CartItem.tsx
@@ -1,16 +1,11 @@
 import React from "react";
-import styled from "styled-components";
 import Header from "../../components/Header";
 import Button from "../../components/Button";
 import Box from "../../components/Box";
+import FlexBox from "../../components/FlexBox";
 import { Item } from "../../domain/ShoppingCart";
 
 // TODO: add quantity to cartItem (check styling in course Udemy)
-
-const ListItem = styled.li`
-  margin: 2rem auto;
-  list-style-type: none;
-`;
 
 interface CartItemProps {
   item: Item;
@@ -22,36 +17,44 @@ function CartItem({ item }: CartItemProps) {
   // };
 
   return (
-    <ListItem>
+    <FlexBox justifyContent="space-around">
       <div>
         <Header as="h4" variant="tertiary">
           {item.title}
         </Header>
-        <Box my="0.5rem">{`€ ${item.price}`}</Box>
-        {/* <Box my="0.5rem">{`#: ${item.quantity}`}</Box>
-        <Box my="0.5rem">{`Total: ${item.quantity * item.price}`}</Box> */}
+        <Box
+          my="0.5rem"
+          fontStyle="italic"
+        >{`Unit price: € ${item.price}`}</Box>
+        <Box my="0.5rem" fontWeight="bold">{`Total: € ${
+          item.quantity * item.price
+        }`}</Box>
       </div>
-      <Button
-        type="button"
-        variant="secondary"
-        m="1rem 1rem 1rem 0"
-        width="2.5rem"
-        // TODO: add the remove handler
-        onClick={() => alert("The product should be decreased")}
-      >
-        -
-      </Button>
-      <Button
-        type="button"
-        variant="secondary"
-        m="1rem 1rem 1rem 0"
-        width="2.5rem"
-        // TODO: add the remove handler
-        onClick={() => alert("The product should be increased")}
-      >
-        +
-      </Button>
-    </ListItem>
+
+      <div>
+        <Box my="0.5rem">{`Amount: ${item.quantity}`}</Box>
+        <Button
+          type="button"
+          variant="secondary"
+          m="1rem 1rem 1rem 0"
+          width="2.5rem"
+          // TODO: add the remove handler
+          onClick={() => alert("The product should be decreased")}
+        >
+          -
+        </Button>
+        <Button
+          type="button"
+          variant="secondary"
+          m="1rem 1rem 1rem 0"
+          width="2.5rem"
+          // TODO: add the remove handler
+          onClick={() => alert("The product should be increased")}
+        >
+          +
+        </Button>
+      </div>
+    </FlexBox>
   );
 }
 
