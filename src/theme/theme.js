e64832b9ae8dbb7ca6a8c1d447351c7ddd3dd814
JONAS <jonas.boone@euri.com>
2022-01-27 13:42:55 +0100

    style: added a new theme object that holds the different color options

---
+++ src/theme/theme.js
@@ -0,0 +1,20 @@
+const theme = {
+  colors: {
+    blacks: {
+      primaryBlack: "#0F1523",
+    },
+    whites: {
+      primaryWhite: "#FFFFFF",
+    },
+    blues: {
+      primaryBlue: "#023E8A",
+      secondaryBlue: "#2347FF",
+      tertiaryBlue: "#DCEAF2",
+    },
+    greens: {
+      primaryGreen: "#2A9D8F",
+      secondaryGreen: "#74C69D",
+    },
+  },
+};
+export default theme;
