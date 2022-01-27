8479e43faff0d250792e99bf3cfa91e432988c54
JONAS <jonas.boone@euri.com>
2022-01-27 13:32:24 +0100

    style(App): added background color

--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -5,6 +5,7 @@ const GlobalStyle = createGlobalStyle({
     margin: 0,
     padding: 0,
     fontFamily: "Montserrat, sans-serif",
+    background: "linear-gradient(to left, #DBF6FE,  #E9F7FF, #E5E2FF)",
   },
 });
 
