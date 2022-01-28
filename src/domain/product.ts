67d496e4a47ffef36c73d7e6fdde0bc770ab1dd9
JONAS <jonas.boone@euri.com>
2022-01-28 16:04:31 +0100

    refactor(product): refactored the product interface

--- src/domain/product.ts
+++ src/domain/product.ts
@@ -1,36 +1,6 @@
+// eslint-disable-next-line import/no-cycle
 import { ProductDTO } from "../api/productsApi";
 
-export interface Product extends Omit<ProductDTO, "createdAt"> {
-  createdAt: Date;
+export interface Product extends ProductDTO {
+  // no additional fields required
 }
-
-// class Product {
-//   constructor(dto?: ProductDTO) {
-//     Object.assign(this, dto, {
-//       createdAt: new Date(dto.createdAt);
-//     })
-//   }
-
-//   copyFrom(source: Product) {
-//     Object.assign(this, source)
-//   }
-// }
-
-// const product = new Product(dto);
-
-// const str = JSON.stringify(product)
-
-// const pro = new Product()
-// pro.copyFrom(JSON.parse(str))
-
-// export interface Product {
-//   id: number;
-//   sku: string;
-//   title: string;
-//   desc: string;
-//   image: string;
-//   stocked: boolean;
-//   basePrice: number;
-//   price: number;
-//   createdAt: Date;
-// }
