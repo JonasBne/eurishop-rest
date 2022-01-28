47bdc859d270833a3d891122c9bf06b0985feb15
JONAS <jonas.boone@euri.com>
2022-01-28 16:23:30 +0100

    refactor(shoppingCart): added interfaces for the shopping cart entity

--- src/domain/basket.ts
+++ src/domain/basket.ts
@@ -1,17 +1,11 @@
 import { ProductDTO } from "../api/productsApi";
 
-// TODO: is this a good place to store this interface (since it's reused in several components such as Home, ShoppingCart, CartItem)
-export interface Item extends ProductDTO {
-  quantity: number;
-}
-
-// TODO: is this a good place to store this interface (since it's reused in several components such as Home, ShoppingCart, CartItem)
-export interface BasketItem {
+export interface CartItem {
   product: ProductDTO;
   quantity: number;
 }
 
-export interface Basket {
-  items: BasketItem[];
+export interface ShoppingCart {
+  items: CartItem[];
   discount: number;
 }
