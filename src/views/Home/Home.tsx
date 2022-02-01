4d14fdfe57adfad670975bebe78c2246408967fc
JONAS <jonas.boone@euri.com>
2022-02-01 14:46:57 +0100

    refactor(Home): cleanup

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,46 +1,13 @@
 import React, { useEffect } from 'react';
 import { useGetProducts } from '../../api/productsApi';
-import Product from '../../domain/product';
 import ErrorModal from '../../components/ErrorModal/ErrorModal';
 import FlexBox from '../../components/FlexBox';
 import LoadingSpinner from '../../components/LoadingSpinner';
 import ProductCard from './ProductCard';
 import ShoppingCart from '../ShoppingCart/ShoppingCart';
-import { Cart, CartItem } from '../../domain/shoppingCart';
 import { useGetBasket, useUpdateBasket } from '../../api/basketApi';
 import rootUrl from '../../api/rootUrl';
 import toasts from '../../components/toasts';
-
-export function addProductToCart(cart: Cart | undefined, product: Product): Cart {
-  if (cart && cart.items.find((item) => item.product.id === product.id)) {
-    return {
-      items: cart.items.map((item) => (item.product.id === product.id
-        ? { ...item, quantity: item.quantity + 1 } : item)),
-    };
-  } if (cart) {
-    return {
-      items: [...cart.items, { product, quantity: 1 }],
-    };
-  }
-  return {
-    items: [],
-  };
-}
-
-// eslint-disable-next-line max-len
-export function updateProductQuantityInCart(cart: Cart | undefined, cartItem: CartItem, action: string) {
-  if (cart && action.includes('+')) {
-    return {
-      items: cart.items.map((item) => (item.product.id === cartItem.product.id
-        ? { ...item, quantity: item.quantity + 1 } : item)),
-    };
-  } if (cart && action.includes('-')) {
-    return {
-      items: cart.items.map((item) => (item.product.id === cartItem.product.id
-        ? { ...item, quantity: item.quantity - 1 } : item)),
-    };
-  } return cart;
-}
 
 function Home() {
   const { succesToast, failToast } = toasts();
