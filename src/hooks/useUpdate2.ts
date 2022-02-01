82ecdb8956bfdc2f1aa6c1a1503b7181e61b3bfc
JONAS <jonas.boone@euri.com>
2022-02-01 13:50:45 +0100

    refactor(useUpdate): remove console log

--- src/hooks/useUpdate2.ts
+++ src/hooks/useUpdate2.ts
@@ -14,7 +14,6 @@ function useUpdate2<T>() {
   // TODO: find a better way to pass the url for basket
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const sendHttpRequest = async (method: UpdateMethods, url: string, data?: T | null, id?: string | number) => {
-    console.log(data);
     try {
       setLoading(true);
       const response = await fetch(url, {
