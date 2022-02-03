cdf5ff98b1afd961ac755a7bd181afbf67eca05b
JONAS <jonas.boone@euri.com>
2022-02-03 14:52:43 +0100

    refactor(productsApi): rewrote useGetMultipleProducts with useQueries

--- src/api/productsApi.ts
+++ src/api/productsApi.ts
@@ -1,11 +1,10 @@
 /* eslint-disable max-len */
 /* eslint-disable import/no-cycle */
 import { useMemo } from 'react';
-import { useQuery } from 'react-query';
+import { useQuery, useQueries } from 'react-query';
 import rootUrl from './rootUrl';
 import Product from '../domain/product';
 import useUpdate from '../hooks/useUpdate';
-import useFetchMultiple from '../hooks/useFetchMultiple';
 import RequestError from '../errors/RequestError';
 
 export interface ProductDTO {
@@ -90,17 +89,33 @@ export const useGetProducts = (page?: number) => {
   };
 };
 
+// export const useGetMultipleProducts = (productIds: string[] | number[]) => {
+//   const urls = useMemo(() => productIds.map((productId) => `${rootUrl}${productUrl}/${productId}`), [rootUrl, productUrl, productIds]);
+
+//   const {
+//     loading, error, data,
+//   } = useFetchMultiple<Product[]>(urls);
+
+//   return {
+//     loading,
+//     error,
+//     products: data,
+//   };
+// };
+
 export const useGetMultipleProducts = (productIds: string[] | number[]) => {
   const urls = useMemo(() => productIds.map((productId) => `${rootUrl}${productUrl}/${productId}`), [rootUrl, productUrl, productIds]);
 
-  const {
-    loading, error, data,
-  } = useFetchMultiple<Product[]>(urls);
+  const productQueries = useQueries(urls.map((url) => ({
+    queryKey: ['product', url],
+    queryFn: () => fetchData(url),
+  })));
+
+  // TODO: how to destructure the isLoading and error props?
+  const products: Product[] = productQueries.map((product) => product.data);
 
   return {
-    loading,
-    error,
-    products: data,
+    products,
   };
 };
 
