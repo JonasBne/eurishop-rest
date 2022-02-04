eca229c7e47bc0704d5f9180e98c6b43e067b636
JONAS <jonas.boone@euri.com>
2022-02-04 14:29:33 +0100

    refactor(fetchHelper): added fetch helper functions

--- src/api/fetchHelper.ts
+++ src/api/fetchHelper.ts
@@ -1,20 +1,35 @@
-// TODO: dit is een herschreven sendhttprequest uit de useUpdate hook
-const request = (methid: string, data: any) => {
+import RequestError from '../errors/RequestError';
 
-} 
+type HttpRequestType = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
 
-export const get = () => { return request('GET')}
-export const put = () => {}
-export const post = () => {}
-export const delete= () => {}
+// TODO: find way to avoid any
+
+const request = async (requestType: HttpRequestType, url: string, data?: any) => {
+  const response = await fetch(url, {
+    method: requestType,
+    headers: data && {
+      'Content-Type': 'application/json',
+    },
+    body: data && JSON.stringify(data),
+  });
+  if (!response.ok) {
+    throw new RequestError(response.status);
+  }
+  return response.json();
+};
+
+export const get = (url: string) => request('GET', url);
+export const put = (url: string, data?: any) => request('PUT', url, data);
+export const post = (url: string, data?: any) => request('POST', url, data);
+export const remove = (url: string) => request('DELETE', url);
 
 export default {
   get,
   post,
-  put
-}
+  put,
+};
 
-
-import api from 'fetchHelper'
-
-api.get<ProductDTO>(url)
+/*
+import api from 'fetchHelper';
+api.get<ProductDTO>(url);
+*/
