46c688d96df9f5934bcbd311958668915a046061
JONAS <jonas.boone@euri.com>
2022-02-04 15:29:56 +0100

    test: moved tests regarding shopping cart to a separate file under domain folder and removed redundant files

--- src/views/ShoppingCart/BasketItem.spec.ts
+++
@@ -1,9 +0,0 @@
-import { CartItem, calculateTotalCostPerCartItem } from '../../domain/shoppingCart';
-
-describe('calculate total cost per cart item', () => {
-  test('multiply price by quantity', () => {
-    const cartItem = { product: { price: 13.00 }, quantity: 2 } as CartItem;
-    const result = calculateTotalCostPerCartItem(cartItem);
-    expect(result).toBe(26.00);
-  });
-});
