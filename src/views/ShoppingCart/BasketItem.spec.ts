9463b55419689e9e35a52ea921264e283d3bb115
JONAS <jonas.boone@euri.com>
2022-02-04 15:27:31 +0100

    test: made code more concise

--- src/views/ShoppingCart/BasketItem.spec.ts
+++ src/views/ShoppingCart/BasketItem.spec.ts
@@ -2,12 +2,7 @@ import { CartItem, calculateTotalCostPerCartItem } from '../../domain/shoppingCa
 
 describe('calculate total cost per cart item', () => {
   test('multiply price by quantity', () => {
-    const cartItem = {
-      product: {
-        price: 13.00,
-      },
-      quantity: 2,
-    } as CartItem;
+    const cartItem = { product: { price: 13.00 }, quantity: 2 } as CartItem;
     const result = calculateTotalCostPerCartItem(cartItem);
     expect(result).toBe(26.00);
   });
