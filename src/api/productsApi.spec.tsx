5db71939833426241ee1d8fdbf1ad520d0f7b5a2
JONAS <jonas.boone@euri.com>
2022-02-08 17:33:13 +0100

    test: cleanup

--- src/api/productsApi.spec.tsx
+++ src/api/productsApi.spec.tsx
@@ -3,28 +3,9 @@
 /* eslint-disable max-len */
 import 'whatwg-fetch';
 import { renderHook } from '@testing-library/react-hooks';
-import { rest } from 'msw';
-import { setupServer } from 'msw/node';
 import { waitFor } from '@testing-library/react';
 import { useGetProduct } from './productsApi';
-import { createWrapper } from '../setupTests';
-
-const server = setupServer(
-  rest.get('https://euricom-test-api.herokuapp.com/api/products/:productId', (req, res, ctx) =>
-    res(
-      ctx.json({
-        id: 1,
-        sku: '254267942-8',
-        title: 'pellentesque',
-        desc: 'Donec posuere metus vitae ipsum.',
-        image: 'https://dummyimage.com/300x300.jpg/ff4444/ffffff',
-        stocked: true,
-        basePrice: 16.63,
-        price: 16.63,
-      }),
-    ),
-  ),
-);
+import { createWrapper, server } from '../setupTests';
 
 beforeAll(() => {
   server.listen({ onUnhandledRequest: 'error' });
