a2ef23c5473ab6321d798788b67b87c90696932b
JONAS <jonas.boone@euri.com>
2022-02-08 16:33:12 +0100

    build: added whatwg-fetch

--- src/api/productsApi.spec.tsx
+++ src/api/productsApi.spec.tsx
@@ -1,1 +1,18 @@
-import { renderHook, act } from '@testing-library/react-hooks';
+/* eslint-disable max-len */
+import 'whatwg-fetch';
+import React, { ReactNode } from 'react';
+import { renderHook } from '@testing-library/react-hooks';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { useGetProduct } from './productsApi';
+
+interface WrapperProps {
+  children: ReactNode;
+}
+
+const client = new QueryClient();
+
+test('renders hook', () => {
+  const wrapper = ({ children }: WrapperProps) => <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  const { result } = renderHook(() => useGetProduct('1'), { wrapper });
+  console.log(result.current);
+});
