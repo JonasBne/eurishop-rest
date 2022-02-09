660a640c87748c0f3d4adb3016597cf9e9fcb398
JONAS <jonas.boone@euri.com>
2022-02-09 09:17:29 +0100

    refactor: added status code to testUtils

--- src/api/productsApi.spec.tsx
+++ src/api/productsApi.spec.tsx
@@ -19,6 +19,16 @@ test('succesful query single product', async () => {
   const { result } = renderHook(() => useGetProduct('1'), { wrapper: createWrapper() });
 
   await waitFor(() => expect(result.current.product).toBeDefined());
+  expect(result.current.product).toEqual({
+    id: 1,
+    sku: '254267942-8',
+    title: 'pellentesque',
+    desc: 'Donec posuere metus vitae ipsum.',
+    image: 'https://dummyimage.com/300x300.jpg/ff4444/ffffff',
+    stocked: true,
+    basePrice: 16.63,
+    price: 16.63,
+  });
 });
 
 /*
--- src/testUtils.ts
+++ src/testUtils.ts
@@ -5,10 +5,11 @@ import { rest } from 'msw';
 const handlers = [
   rest.get('https://euricom-test-api.herokuapp.com/api/products/:productId', (req, res, ctx) =>
     res(
+      ctx.status(200),
       ctx.json({
         id: 1,
         sku: '254267942-8',
-        title: 'pellentesque',
+        title: 'test',
         desc: 'Donec posuere metus vitae ipsum.',
         image: 'https://dummyimage.com/300x300.jpg/ff4444/ffffff',
         stocked: true,
