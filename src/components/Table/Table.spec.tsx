1dbc4fa48f221e92ec704dd46264809417e15571
JONAS <jonas.boone@euri.com>
2022-02-09 14:28:38 +0100

    test: added a test for table component

--- src/components/Table/Table.spec.tsx
+++ src/components/Table/Table.spec.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
 import userEvent from '@testing-library/user-event';
 import Product from '../../domain/product';
-import Table from '../../components/Table/Table';
+import Table from './Table';
 import theme from '../../theme/theme';
 
 // TODO: move to table.spec.tsx
@@ -11,92 +11,100 @@ import theme from '../../theme/theme';
 
 // TODO: test aantal rijen in tabel met aantal rijen van producten api
 
-// TODO: verplaatsen in beforeEach
-const sortExpression = '';
-const columns = [
-  {
-    name: 'id',
-    label: 'Product ID',
-    sortable: true,
-    id: 'col1',
-  },
-  {
-    name: 'sku',
-    label: 'Product number',
-    sortable: true,
-    id: 'col2',
-  },
-  {
-    name: 'title',
-    label: 'Title',
-    sortable: true,
-    id: 'col3',
-  },
-  {
-    name: 'desc',
-    label: 'Description',
-    sortable: false,
-    id: 'col4',
-  },
-  {
-    name: 'image',
-    label: 'Image URL',
-    sortable: false,
-    id: 'col5',
-  },
-  {
-    name: 'stocked',
-    label: 'In stock',
-    sortable: true,
-    id: 'col6',
-  },
-  {
-    name: 'basePrice',
-    label: 'Base price',
-    sortable: true,
-    id: 'col7',
-  },
-  {
-    name: 'price',
-    label: 'Unit price',
-    sortable: true,
-    id: 'col8',
-  },
-  {
-    name: 'actions',
-    label: 'Actions',
-    sortable: false,
-    id: 'col9',
-  },
-];
-const products = [
-  {
-    id: 1,
-    title: 'product1',
-    sku: 'AAA',
-    basePrice: 10.0,
-    price: 10.0,
-    image: 'https://dummyimage.com',
-    desc: 'description product1',
-  },
-  {
-    id: 2,
-    title: 'product2',
-    sku: 'BBB',
-    basePrice: 20.0,
-    price: 20.0,
-    image: 'https://dummyimage.com',
-    desc: 'description product2',
-  },
-] as Product[];
-
-const mockSetSortExpression = jest.fn();
-const mockOnLoad = jest.fn();
-const mockOnRowClick = jest.fn();
-const mockOnActionClick = jest.fn();
-
 describe('product list', () => {
-  beforeEach(() => {});
+  let sortExpression: string;
+  let columns: any[];
+  let products: Product[];
+
+  let mockSetSortExpression: () => void;
+  let mockOnLoad: () => void;
+  let mockOnRowClick: () => void;
+  let mockOnActionClick: () => void;
+
+  beforeEach(() => {
+    sortExpression = '';
+    products = [
+      {
+        id: 1,
+        title: 'product1',
+        sku: 'AAA',
+        basePrice: 10.0,
+        price: 10.0,
+        image: 'https://dummyimage.com',
+        desc: 'description product1',
+      },
+      {
+        id: 2,
+        title: 'product2',
+        sku: 'BBB',
+        basePrice: 20.0,
+        price: 20.0,
+        image: 'https://dummyimage.com',
+        desc: 'description product2',
+      },
+    ] as Product[];
+    columns = [
+      {
+        name: 'id',
+        label: 'Product ID',
+        sortable: true,
+        id: 'col1',
+      },
+      {
+        name: 'sku',
+        label: 'Product number',
+        sortable: true,
+        id: 'col2',
+      },
+      {
+        name: 'title',
+        label: 'Title',
+        sortable: true,
+        id: 'col3',
+      },
+      {
+        name: 'desc',
+        label: 'Description',
+        sortable: false,
+        id: 'col4',
+      },
+      {
+        name: 'image',
+        label: 'Image URL',
+        sortable: false,
+        id: 'col5',
+      },
+      {
+        name: 'stocked',
+        label: 'In stock',
+        sortable: true,
+        id: 'col6',
+      },
+      {
+        name: 'basePrice',
+        label: 'Base price',
+        sortable: true,
+        id: 'col7',
+      },
+      {
+        name: 'price',
+        label: 'Unit price',
+        sortable: true,
+        id: 'col8',
+      },
+      {
+        name: 'actions',
+        label: 'Actions',
+        sortable: false,
+        id: 'col9',
+      },
+    ];
+    mockSetSortExpression = jest.fn();
+    mockOnLoad = jest.fn();
+    mockOnRowClick = jest.fn();
+    mockOnActionClick = jest.fn();
+  });
+
   test('click fires onRowClick event', () => {
     render(
       <ThemeProvider theme={theme}>
