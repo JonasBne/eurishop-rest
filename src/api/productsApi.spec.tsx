e4ebab09626c7a2f4145b6d1237befdef5c7f537
JONAS <jonas.boone@euri.com>
2022-02-09 11:17:41 +0100

    test: added test suite for useGetProduct

--- src/api/productsApi.spec.tsx
+++ src/api/productsApi.spec.tsx
@@ -10,32 +10,36 @@ import { server } from '../setupTests';
 import { createWrapper } from '../testUtils';
 import RequestError from '../errors/RequestError';
 
-test('succesful query single product', async () => {
-  const { result } = renderHook(() => useGetProduct('1'), { wrapper: createWrapper() });
+describe('single product', () => {
+  test('succesful query returns product', async () => {
+    const { result } = renderHook(() => useGetProduct('1'), { wrapper: createWrapper() });
 
-  await waitFor(() => expect(result.current.product).toBeDefined());
-  expect(result.current.product).toEqual({
-    id: 1,
-    sku: '254267942-8',
-    title: 'pellentesque',
-    desc: 'Donec posuere metus vitae ipsum.',
-    image: 'https://dummyimage.com/300x300.jpg/ff4444/ffffff',
-    stocked: true,
-    basePrice: 16.63,
-    price: 16.63,
+    await waitFor(() => expect(result.current.product).toBeDefined());
+    expect(result.current.product).toEqual({
+      id: 1,
+      sku: '254267942-8',
+      title: 'pellentesque',
+      desc: 'Donec posuere metus vitae ipsum.',
+      image: 'https://dummyimage.com/300x300.jpg/ff4444/ffffff',
+      stocked: true,
+      basePrice: 16.63,
+      price: 16.63,
+    });
   });
-});
 
-test('failed query single products', async () => {
-  server.use(
-    rest.get('https://euricom-test-api.herokuapp.com/api/products/:productId', (req, res, ctx) => res(ctx.status(404))),
-  );
+  test('failed query returns RequestError ', async () => {
+    server.use(
+      rest.get('https://euricom-test-api.herokuapp.com/api/products/:productId', (req, res, ctx) =>
+        res(ctx.status(404)),
+      ),
+    );
 
-  const { result } = renderHook(() => useGetProduct('aaaa'), { wrapper: createWrapper() });
+    const { result } = renderHook(() => useGetProduct('aaaa'), { wrapper: createWrapper() });
 
-  await waitFor(() => expect(result.current.isError).toBeTruthy());
+    await waitFor(() => expect(result.current.isError).toBeTruthy());
 
-  expect(result.current.error).toBeInstanceOf(RequestError);
+    expect(result.current.error).toBeInstanceOf(RequestError);
+  });
 });
 
 /*
