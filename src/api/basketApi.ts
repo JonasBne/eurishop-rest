8b65b04d51a8f512b1342fa00c5c5ad3023b97fc
JONAS <jonas.boone@euri.com>
2022-02-10 11:10:00 +0100

    feat: added useMutationBasketPost

--- src/api/basketApi.ts
+++ src/api/basketApi.ts
@@ -1,5 +1,6 @@
+/* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable max-len */
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient, useMutation } from 'react-query';
 import Product from '../domain/product';
 import { CartItem, Cart } from '../domain/shoppingCart';
 import api from './fetchHelper';
@@ -17,18 +18,38 @@ export const basketUrls = {
   update: 'api/basket/xyz/product',
 };
 
+interface PostBasketVariables {
+  quantity: number;
+  productId: string | number;
+}
+
+const postItemToBasket = async (productId: string | number, data: any) =>
+  api.post(`${rootUrl}${basketUrls.update}/${productId}`, data);
+
+const patchBasket = async (productId: string | number, data: any) =>
+  api.patch(`${rootUrl}${basketUrls.update}/${productId}`, data);
+
+const removeItemFromBasket = async (productId: string | number) =>
+  api.remove(`${rootUrl}${basketUrls.update}/${productId}`);
+
 export const basketMapper = (products?: Product[], basketDTO?: BasketDTO[]): Cart | undefined => {
   if (!basketDTO || !products) return undefined;
 
-  const cartItems: CartItem[] = products.map((product) => (
-    { product, quantity: basketDTO.find((item) => item.productId === product.id)!.quantity }));
+  const cartItems: CartItem[] = products.map((product) => ({
+    product,
+    quantity: basketDTO.find((item) => item.productId === product.id)!.quantity,
+  }));
   return {
     items: cartItems,
   };
 };
 
 export const useGetBasket = () => {
-  const { data, refetch: cartRefetch } = useQuery<BasketDTO[]>(['basket'], () => api.get(`${rootUrl}${basketUrls.base}`), { keepPreviousData: true });
+  const { data, refetch: cartRefetch } = useQuery<BasketDTO[]>(
+    ['basket'],
+    () => api.get(`${rootUrl}${basketUrls.base}`),
+    { keepPreviousData: true },
+  );
 
   const productIds = data?.map((cartItem) => cartItem.productId) ?? [];
 
@@ -38,4 +59,16 @@ export const useGetBasket = () => {
     cart: basketMapper(products, data),
     cartRefetch,
   };
+};
+
+export const useMutationBasketPost = () => {
+  const queryClient = useQueryClient();
+  return useMutation<BasketDTO, Error, PostBasketVariables>(
+    ({ quantity, productId }) => postItemToBasket(quantity, productId),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries('basket');
+      },
+    },
+  );
 };
